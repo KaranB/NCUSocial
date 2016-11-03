905e0a4b0569849c66187268e066fa1f111a9e52
dbhrockzz <dbhrockzz1@gmail.com>
2016-11-03 17:31:22 +0530

    Login and category table API Added

---
+++ dbase/category.java
@@ -0,0 +1,81 @@
+package dbase;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;
+
+public class category {
+	private static final String dbClassName = "com.mysql.jdbc.Driver";
+	private static final String CONNECTION =
+            "jdbc:mysql://127.0.0.1/ncu_social";
+	
+	public Connection makeConnection() throws ClassNotFoundException,SQLException
+	{
+		Class.forName(dbClassName);
+		Connection con = DriverManager.getConnection(CONNECTION,"root","dbhrockzz");
+		System.out.println("Connected to Database");
+		return con;
+		
+	}
+	
+	public boolean addCategory(String category_name) throws ClassNotFoundException,SQLException
+	{
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("INSERT INTO category(category_name) VALUES(?);");
+		stmt.setString(1,category_name);
+		stmt.execute();
+		System.out.println("Category Added Successfully");
+		con.close();
+		return true;
+	}
+	
+	public boolean delCategory(String category_name) throws ClassNotFoundException, SQLException
+	{
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("DELETE FROM category WHERE category_name=?");
+		stmt.setString(1,category_name);
+		stmt.execute();
+		System.out.println("Category Deleted Successfully");
+		con.close();
+		return true;
+	}
+	
+	public String[] categories() throws ClassNotFoundException,SQLException
+	{
+		Connection con = makeConnection();
+		Statement stmt = con.createStatement();
+		ResultSet rs = stmt.executeQuery("SELECT category_name from category;");
+		List<String> results = new ArrayList<String>();
+		while(rs.next())
+		{
+			results.add(rs.getString("category_name"));
+		}
+		String arr[] = new String[results.size()];
+		int i = 0;
+		for(Object a : results.toArray())
+		{
+			arr[i] = new String((String)a);
+			i++;
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args)
+	{
+		category obj = new category();
+		try{
+			String arr[] = obj.categories();
+			for(String a:arr)
+			{
+				System.out.println(a);
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ dbase/login.java
@@ -0,0 +1,87 @@
+package dbase;
+
+import java.sql.*;
+public class login {
+	
+	private static final String dbClassName = "com.mysql.jdbc.Driver";
+	private static final String CONNECTION =
+            "jdbc:mysql://127.0.0.1/ncu_social";
+	
+	public Connection makeConnection() throws ClassNotFoundException,SQLException
+	{
+		Class.forName(dbClassName);
+		Connection con = DriverManager.getConnection(CONNECTION,"root","dbhrockzz");
+		System.out.println("Connected to Database");
+		return con;
+		
+	}
+	public boolean addUser(String username,String password) throws SQLException, ClassNotFoundException{
+		
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("INSERT INTO login(username,password) VALUES(?,?);");
+		stmt.setString(1, username);
+		stmt.setString(2,password);
+		stmt.execute();
+		System.out.println("User Added Successfully");
+		con.close();
+		return true;
+	}
+	
+	public boolean delUser(String username) throws SQLException, ClassNotFoundException
+	{
+		
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("DELETE FROM login WHERE username=?");
+		stmt.setString(1, username);
+		stmt.execute();
+		System.out.println("User Deleted Successfully");
+		con.close();
+		return true;
+	}
+	
+	public boolean updateUser(String username,String password) throws SQLException, ClassNotFoundException
+	{
+		
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("UPDATE login SET password=? WHERE username=?");
+		stmt.setString(1, password);
+		stmt.setString(2, username);
+		stmt.execute();
+		System.out.println("User Updated Successfully");
+		con.close();
+		return true;
+	}
+
+	public boolean checkCredentials(String username, String password) throws SQLException, ClassNotFoundException
+	{
+		Connection con = makeConnection();
+		PreparedStatement stmt = con.prepareStatement("SELECT password FROM login WHERE username=?");
+		stmt.setString(1, username);
+		ResultSet rs = stmt.executeQuery();
+		rs.next();
+		if(password.equals(rs.getString(1))){
+			con.close();
+			return true;
+		}
+		con.close();
+		return false;
+	}
+	
+//	public static void main(String [] args)
+//	{
+//		login obj =  new login();
+//		try{
+//			if(obj.checkCredentials("dbh","hellonow"))
+//			{
+//				System.out.println("Verified");
+//			}
+//			else{
+//				System.out.println("Rejected");
+//			}
+//		}
+//		catch(Exception e){
+//			e.printStackTrace();
+//		}
+//	}
+
+}
